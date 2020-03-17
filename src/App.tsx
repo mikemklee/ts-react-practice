fb2496a186b6c5ad1a86abdc5edbacbba703ddbf
Mike Lee <mikemunkyu.lee@gmail.com>
2020-03-16 22:00:40 -0400

    Initial setup for a barebones todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+
 function App() {
-  return <div className="App"></div>;
+  const todos = [{ id: 't1', text: 'finish this course' }];
+
+  return (
+    <div className="App">
+      <TodoList items={todos} />
+    </div>
+  );
 }
 
 export default App;
